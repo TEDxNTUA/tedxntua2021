e73018ed853c403a04bc8037c7876fe1bec89ee2
nickbel7 <nikolas.bellos@gmail.com>
2021-04-04 19:40:02 +0300

    Removed junk files / added partners to bundles.base

--- project/partners/assets/js/partners.js
+++ project/partners/assets/js/partners.js
@@ -1,15 +1,15 @@
 import $ from 'jquery'
 
-// const leafletFrame = document.getElementById('leaflet-iframe')
+const leafletFrame = document.getElementById('leaflet-iframe')
 
-// $(document).ready(function () {
-//     $('#leaflet-modal').on('show.bs.modal', function (event) {
-//         var btn = event.relatedTarget
-//         var leafletPath = btn.dataset.leafletUrl
-//         // Update img of modal
-//         leafletFrame.setAttribute('src', leafletPath + '#view=Fit')
-//     })
-// })
+$(document).ready(function () {
+    $('#leaflet-modal').on('show.bs.modal', function (event) {
+        var btn = event.relatedTarget
+        var leafletPath = btn.dataset.leafletUrl
+        // Update img of modal
+        leafletFrame.setAttribute('src', leafletPath + '#view=Fit')
+    })
+})
 
 var activePartner;
 
