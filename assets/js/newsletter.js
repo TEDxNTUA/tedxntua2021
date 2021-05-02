05343629d7f7aefb45bb4498f3cab3aaca0bf0ec
nickbel7 <nikolas.bellos@gmail.com>
2021-05-02 17:05:50 +0300

    Pre launch bug fixes, added dialogues card in home

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -4,15 +4,15 @@ var submitButton = document.getElementById('newsletter-submit-btn')
 var emailField = document.getElementById('newsletter-input-field')
 
 submitButton.onclick = function(){
+    let m = document.getElementById("newsletter-modal");
+    $(m).removeClass("hidden");
+    setTimeout(function() { $(m).addClass("hidden"); }, 2000);
     $.ajax({
         url:'https://api.apispreadsheets.com/data/9080/',
         type:'post',
         data:$("#newsletter-form").serializeArray(),
         success: function(){
           // alert("SUCCESS! Welcome to our Newsletter")
-          let m = document.getElementById("newsletter-modal");
-          $(m).removeClass("hidden");
-          setTimeout(function() { $(m).addClass("hidden"); }, 1000);
         },
         error: function(){
           alert("There was an error :(")
